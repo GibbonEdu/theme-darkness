37702ac3fea1ab326af25e194335787f294a7e35
Ross Parler <rparker@MBP00012-Ross-Parker-2.local>
2014-08-30 19:50:59 +0800

    v1.0.04

--- manifest.php
+++ manifest.php
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //Basica variables
 $name="Darkness" ;
 $description="A darker Gibbon" ;
-$version="1.0.03" ;
+$version="1.0.04" ;
 $author="Ross Parker" ;
 $url="http://rossparker.org" ;
 ?>
